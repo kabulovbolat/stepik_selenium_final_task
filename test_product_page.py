0be48cc387a1858dabdf2a4e2a43f5df6ae18dfc
kabulovbolat <kaboul777@gmail.com>
2020-06-09 19:55:31 +0600

    1

---
+++ test_product_page.py
@@ -0,0 +1,111 @@
+import pytest
+import time
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+
+LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+LOGIN_LINK = "http://selenium1py.pythonanywhere.com/accounts/login/"
+
+
+class TestUserAddToBasketFromProductPage(object):
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        page = LoginPage(browser, LOGIN_LINK)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        page.register_new_user(email, "t,fnmvjq[eq")
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, LINK)
+        page.open()
+        page.check_adding_message_not_present()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, LINK)
+        page.open()
+        page.add_product_to_basket()
+        page.check_for_message_product_is_added()
+        page.check_for_correct_basket_price()
+
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                    pytest.param(
+                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                    marks=pytest.mark.xfail(reason="bugged")
+                                    ),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser,link):
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.check_for_message_product_is_added()
+    page.check_for_correct_basket_price()
+
+@pytest.mark.xfail(reason="broken by design")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.check_adding_message_not_present()
+
+@pytest.mark.xfail(reason="broken by design")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_product_to_basket()
+    page.should_be_success_message()
+    page.success_message_should_disappear()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_url()
+    basket_page.basket_is_empty()
+    basket_page.should_be_empty_basket_message()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.go_to_login_page()
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_login_url()
+    page.should_be_login_form()
